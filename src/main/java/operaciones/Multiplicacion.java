303e18e1c66fc8ac9aa569ab57452ad1b88bceab
mmaurogg <mmaurogg@gmail.com>
2022-05-25 22:56:41 -0500

    agregada documentación faltante

--- src/main/java/operaciones/Multiplicacion.java
+++ src/main/java/operaciones/Multiplicacion.java
@@ -1,30 +1,60 @@
 package operaciones;
 
+/**
+ * Esta clase realiza la multiplicación de dos números recibidos
+ * @author Juan Ramallo y Mauricio Gómez
+ * @version 1.0.0
+ */
 public class Multiplicacion {
     private double multiplicando;
     private double multiplicador;
 
+    /**
+     * Constructor para inicializar la operación multiplicación
+     * @param multiplicando número que se está multiplicando
+     * @param multiplicador número por el que se está multiplicando
+     */
     public Multiplicacion(double multiplicando, double multiplicador) {
         this.multiplicando = multiplicando;
         this.multiplicador = multiplicador;
     }
 
+    /**
+     * Método que devuelve el valor del multiplicando
+     * @return valor almacenado del multiplicando
+     */
     public double getMultiplicando() {
         return multiplicando;
     }
 
+    /**
+     * Método que devuelve el valor del multiplicador
+     * @return valor almacenado del multiplicador
+     */
     public double getMultiplicador() {
         return multiplicador;
     }
 
+    /**
+     * Metodo que recibe el valor del multiplicador
+     * @param multiplicador número que se está multiplicando
+     */
     public void setMultiplicador(double multiplicador) {
         this.multiplicador = multiplicador;
     }
 
+    /**
+     * Metodo que recibe el valor del multiplicando
+     * @param multiplicando número por el que se está multiplicando
+     */
     public void setMultiplicando(double multiplicando) {
         this.multiplicando = multiplicando;
     }
 
+    /**
+     * Método que realiza la multiplicación
+     * @return el resultado de la multiplicación en formato double
+     */
     public double multiplicar() {
         return multiplicando * multiplicador;
     }
