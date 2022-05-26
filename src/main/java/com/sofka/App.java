dfa306b6c9f046058c4bd29dbbef4f43f95f0feb
juanir33 <guaca33@gmail.com>
2022-05-26 01:34:53 -0300

    Modifique App.java

--- src/main/java/com/sofka/App.java
+++ src/main/java/com/sofka/App.java
@@ -1,15 +1,16 @@
 package com.sofka;
 
+
+import com.sofka.operaciones.*;
+
+
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.sofka.operaciones.Resta;
-import com.sofka.operaciones.Suma;
-import com.sofka.operaciones.Multiplicacion;
-import com.sofka.operaciones.Division;
-
 public class App {
+
+
 
     public static void main(String[] args) {
 
@@ -17,18 +18,21 @@ public class App {
 
     }
 
+    private static final String MGSRESULTADO = "El resultado es {0}";
+    private static final String MGSCONTINUAR = "¿Desea continuar? (y/n)";
+
     public static void menu() {
 
         Logger log = Logger.getLogger("imprimir");
         log.info("\nBienvenido a la calculadora de Sofka");
 
-        int respuesta = 0;
+        int respuesta;
         do {
             Scanner scanner = new Scanner(System.in);
 
             log.info("Seleccione la operación que desea hacer: ");
             log.info("\n1. suma\n2. Resta\n3. Multiplicación\n4. División\n0. salir");
-            respuesta = Integer.valueOf(scanner.nextLine());
+            respuesta = Integer.parseInt(scanner.nextLine());
 
             log.info("Ingrese el primer número: ");
             double primerNumero = Double.parseDouble(scanner.nextLine());
@@ -39,83 +43,44 @@ public class App {
             String resultado;
             String confirmacion;
 
+
             switch (respuesta) {
-
-                case 1:
-
+                case 1 -> {
                     Suma suma = new Suma(primerNumero, segundoNumero);
                     resultado = String.valueOf(suma.suma());
-                    log.log(Level.INFO, "El resultado es {0}", resultado);
-
-                    break;
-
-                case 2:
-
+                    log.log(Level.INFO, MGSRESULTADO, resultado);
+                }
+                case 2 -> {
                     Resta resta = new Resta(primerNumero, segundoNumero);
                     resultado = String.valueOf(resta.resta());
-                    log.log(Level.INFO, "El resultado es {0}", resultado);
-
-                    log.info("¿Desea continuar? (y/n)");
-                    confirmacion = scanner.nextLine();
-
-                    if (!confirmacion.equals("y")) {
-                        respuesta = 0;
-                    }
-
-                    break;
-
-                case 3:
-
+                    log.log(Level.INFO, MGSRESULTADO, resultado);
+                }
+                case 3 -> {
                     Multiplicacion multiplicacion = new Multiplicacion(primerNumero, segundoNumero);
                     resultado = String.valueOf(multiplicacion.multiplicar());
-                    log.log(Level.INFO, "El resultado es {0}", resultado);
-
-                    log.info("¿Desea continuar? (y/n)");
-                    confirmacion = scanner.nextLine();
-
-                    if (!confirmacion.equals("y")) {
-                        respuesta = 0;
-                    }
-
-                    break;
-
-                case 4:
-
+                    log.log(Level.INFO, MGSRESULTADO, resultado);
+                }
+                case 4 -> {
                     Division division = new Division(primerNumero, segundoNumero);
                     resultado = division.dividir();
-                    log.log(Level.INFO, "El resultado es {0}", resultado);
-
-                    log.info("¿Desea continuar? (y/n)");
-                    confirmacion = scanner.nextLine();
-
-                    if (!confirmacion.equals("y")) {
-                        respuesta = 0;
-                    }
-
-                    break;
-
-                    default:
-                    menu();
-                    break;
-
+                    log.log(Level.INFO, MGSRESULTADO, resultado);
+                }
+                default -> log.info("Ingrese una opcion");
+            }
+            log.info(MGSCONTINUAR);
+            confirmacion = scanner.nextLine();
+            if (!confirmacion.equals("y")) {
+                respuesta = 0;
+                log.info("\n\nGracias por usar esta fabulosa calculadora!\n");
             }
 
-            scanner.close();
 
-            log.info("\n\nGracias por usar esta fabulosa calculadora!\n");
+
 
         } while (respuesta != 0);
 
-    }
 
-    private static boolean confirmacion () {
-        Logger log = Logger.getLogger("imprimir");
-        Scanner scanner = new Scanner(System.in);
 
-        log.info("¿Desea continuar? (y/n)");
-        String confirmacion = scanner.nextLine();
-
-        return confirmacion.equals("y");
     }
 
 }
